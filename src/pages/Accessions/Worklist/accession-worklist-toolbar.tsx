866d1af575e87b383c109268f287f0390bbd59dd
Paul DeVito <pdevito3@gmail.com>
2023-08-18 18:53:26 -0400

    debounce work

--- src/pages/Accessions/Worklist/accession-worklist-toolbar.tsx
+++ src/pages/Accessions/Worklist/accession-worklist-toolbar.tsx
@@ -2,26 +2,33 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useDebouncedValue } from "@/hooks/useDebouncedValue";
 import { CircleIcon, TimerIcon, XCircle } from "lucide-react";
+import { useEffect, useState } from "react";
 import { FilterControl } from "./filter-control";
 import { useAccessioningWorklistTableStore } from "./paginated-data-table";
 
 export function AccessionWorklistToolbar() {
-  const {
-    filterInput: filter,
-    setFilterInput: setFilter,
-    isFiltered,
-    resetFilters,
-  } = useAccessioningWorklistTableStore();
+  const { filterInput, setFilterInput, isFiltered, resetFilters } =
+    useAccessioningWorklistTableStore();
+  const [liveValue, setLiveValue] = useState(filterInput);
+  const [debouncedFilterInput] = useDebouncedValue(liveValue, 400);
+
+  useEffect(() => {
+    setFilterInput(debouncedFilterInput);
+    if (debouncedFilterInput === null) {
+      resetFilters();
+    }
+  }, [debouncedFilterInput, resetFilters, setFilterInput]);
 
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center flex-1 space-x-2">
         <Input
           placeholder="Filter accessions..."
-          value={(filter as string) ?? ""}
+          value={(liveValue as string) ?? ""}
           onChange={(event) => {
-            setFilter(event.target.value);
+            setLiveValue(event.currentTarget.value);
           }}
           className="w-48 h-8 lg:w-54"
         />
@@ -29,7 +36,10 @@ export function AccessionWorklistToolbar() {
         {isFiltered.result() && (
           <Button
             variant="ghost"
-            onClick={() => resetFilters()}
+            onClick={() => {
+              setLiveValue(null);
+              resetFilters();
+            }}
             className="h-8 px-2 lg:px-3"
           >
             Reset
