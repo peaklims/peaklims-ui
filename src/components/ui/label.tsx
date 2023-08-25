82944d84b594196a9c86b974693af7fd4933d564
Paul DeVito <pdevito3@gmail.com>
2023-08-24 23:24:58 -0400

    feat: small labels

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
-  "text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
 type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
