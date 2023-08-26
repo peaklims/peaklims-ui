43b18e14d43c719a3825e95f11b9ba1730242613
Paul DeVito <pdevito3@gmail.com>
2023-08-25 23:51:23 -0400

    WIP: accession details section

---
+++ src/components/ui/vertical-tabs.tsx
@@ -0,0 +1,58 @@
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const VerticalTabs = TabsPrimitive.Root;
+
+const VerticalTabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn(
+      "max-h-[20rem] sm:max-h-full flex flex-col items-center justify-start rounded-md sm:rounded-l-md sm:rounded-r-none bg-muted p-2 text-muted-foreground overflow-y-auto",
+      className
+    )}
+    {...props}
+  />
+));
+VerticalTabsList.displayName = TabsPrimitive.List.displayName;
+
+const VerticalTabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      "w-full inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-3 sm:py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      className
+    )}
+    {...props}
+  />
+));
+VerticalTabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+
+const VerticalTabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn(
+      "w-full ring-offset-background rounded-md sm:rounded-r-md sm:rounded-l-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      className
+    )}
+    {...props}
+  />
+));
+VerticalTabsContent.displayName = TabsPrimitive.Content.displayName;
+
+export {
+  VerticalTabs,
+  VerticalTabsContent,
+  VerticalTabsList,
+  VerticalTabsTrigger,
+};
--- src/pages/Accessions/edit-accession-page.tsx
+++ src/pages/Accessions/edit-accession-page.tsx
@@ -1,3 +1,9 @@
+import {
+  VerticalTabs,
+  VerticalTabsContent,
+  VerticalTabsList,
+  VerticalTabsTrigger,
+} from "@/components/ui/vertical-tabs";
 import { useGetAccessionForEdit } from "@/domain/accessions/apis/get-editable-aggregate";
 import { ManageAccessionPatientCard } from "@/domain/patients/components/manage-accession-patient";
 import { useParams } from "@tanstack/react-router";
@@ -30,9 +36,79 @@ export function EditAccessionPage() {
 
       <div className="flex items-center justify-center w-full pt-3 md:block">
         <div className="space-y-10">
-          <ManageAccessionPatientCard accession={accession} />
+          <div>
+            <h2 className="sr-only">Accession Patient</h2>
+            <ManageAccessionPatientCard accession={accession} />
+          </div>
+          <div>
+            {accession?.patient?.id ? (
+              <>
+                <h2 className="text-3xl">Accession Details</h2>
+                <div className="pt-3">
+                  <OrganizationManagement accessionId={accessionId} />
+                </div>
+              </>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function OrganizationManagement({ accessionId }: { accessionId: string }) {
+  return (
+    <VerticalTabs
+      defaultValue="organization"
+      className="sm:flex"
+      orientation="vertical"
+    >
+      <VerticalTabsList>
+        <VerticalTabsTrigger value="organization">
+          Organization
+        </VerticalTabsTrigger>
+        <VerticalTabsTrigger value="samples">Samples</VerticalTabsTrigger>
+        <VerticalTabsTrigger value="panels-and-tests">
+          Panels and Tests
+        </VerticalTabsTrigger>
+        <VerticalTabsTrigger value="attachments">
+          Attachments
+        </VerticalTabsTrigger>
+        <VerticalTabsTrigger value="comments">Comments</VerticalTabsTrigger>
+      </VerticalTabsList>
+      <VerticalTabsContent
+        value="organization"
+        className="h-[50rem] overflow-auto px-6 py-4 bg-rose-200"
+      >
+        <h3 className="text-xl font-semibold tracking-tight">
+          Organization Details
+        </h3>
+        <p>Make changes to your org here.</p>
+      </VerticalTabsContent>
+      <VerticalTabsContent
+        value="samples"
+        className="h-[50rem] overflow-auto px-6 py-4 bg-rose-300"
+      >
+        Make changes to your samples here.
+      </VerticalTabsContent>
+      <VerticalTabsContent
+        value="panels-and-tests"
+        className="h-[50rem] overflow-auto px-6 py-4 bg-rose-400"
+      >
+        Change your panels and tests here.
+      </VerticalTabsContent>
+      <VerticalTabsContent
+        value="attachments"
+        className="h-[50rem] overflow-auto px-6 py-4 bg-rose-500"
+      >
+        Change your attachments here.
+      </VerticalTabsContent>
+      <VerticalTabsContent
+        value="comments"
+        className="h-[50rem] overflow-auto px-6 py-4 bg-rose-600"
+      >
+        Change your comments here.
+      </VerticalTabsContent>
+    </VerticalTabs>
   );
 }
