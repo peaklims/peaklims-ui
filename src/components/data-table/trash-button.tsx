5d03bddf1ea6f5787b0c1d432a67a701758651fc
Paul DeVito <pdevito3@gmail.com>
2023-09-02 17:01:12 -0400

    refactor: minor cleanup

--- src/components/data-table/trash-button.tsx
+++ src/components/data-table/trash-button.tsx
@@ -13,9 +13,9 @@ export function TrashButton({ onClick, hideInGroup = true }: TrashButtonProps) {
       onClick={onClick}
       className={cn(
         "inline-flex items-center px-2 py-2 text-sm font-medium leading-5 transition duration-100 ease-in bg-white border border-gray-300 rounded-full shadow-sm",
-        "hover:bg-red-200 hover:text-red-800 hover:outline-none",
-        "dark:border-slate-900 dark:bg-slate-800 dark:text-white dark:hover:bg-red-800 dark:hover:text-red-300 dark:hover:outline-none",
-        "sm:px-3 sm:py-1 dark:hover:shadow dark:shadow-red-400 dark:hover:shadow-red-300",
+        "hover:bg-rose-200 hover:text-rose-800 hover:outline-none",
+        "dark:border-slate-900 dark:bg-slate-800 dark:text-white dark:hover:bg-rose-800 dark:hover:text-rose-300 dark:hover:outline-none",
+        "sm:px-3 sm:py-1 dark:hover:shadow dark:shadow-rose-400 dark:hover:shadow-rose-300",
         hideInGroup && "sm:opacity-0 sm:group-hover:opacity-100"
       )}
     >
