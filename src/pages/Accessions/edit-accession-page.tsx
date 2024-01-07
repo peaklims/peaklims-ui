9e24c8fb5fb26c2b0e045951838effa0bea4e192
Paul DeVito <pdevito3@gmail.com>
2024-01-07 11:27:07 -0500

    feat: audit log placeholder

--- src/pages/Accessions/edit-accession-page.tsx
+++ src/pages/Accessions/edit-accession-page.tsx
@@ -244,6 +244,36 @@ function AccessionDetails({
           Change your comments here.
         </div>
       </Tab>
+
+      <Tab
+        key="auditlog"
+        title={
+          <div className="flex items-center space-x-2">
+            {/* https://iconbuddy.app/akar-icons/history */}
+            <svg
+              width="512"
+              height="512"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4.266 16.06a8.923 8.923 0 0 0 3.915 3.978a8.706 8.706 0 0 0 5.471.832a8.795 8.795 0 0 0 4.887-2.64a9.067 9.067 0 0 0 2.388-5.079a9.135 9.135 0 0 0-1.044-5.53a8.903 8.903 0 0 0-4.069-3.815a8.7 8.7 0 0 0-5.5-.608c-1.85.401-3.366 1.313-4.62 2.755c-.151.16-.735.806-1.22 1.781M7.5 8l-3.609.72L3 5m9 4v4l3 2"
+              />
+            </svg>
+            <span>Audit Log</span>
+          </div>
+        }
+      >
+        <div className="h-full px-6 py-4 overflow-auto bg-cyan-500">
+          Change your audit log setup here.
+        </div>
+      </Tab>
     </Tabs>
   );
 }
