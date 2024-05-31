19f706817748ac8c9310d8821febd79531516b42
Paul DeVito <pdevito3@gmail.com>
2024-05-30 22:33:07 -0400

    feat: more hotkeys

--- src/domain/accessions/features/worklist/accession-worklist-toolbar.tsx
+++ src/domain/accessions/features/worklist/accession-worklist-toolbar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { Kbd, TooltipHotkey } from "@/components";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useNewAccession } from "@/domain/accessions";
 import { FilterControl } from "@/domain/accessions/features/worklist/accession-worklist-filter-control";
 import { useAccessioningWorklistTableStore } from "@/domain/accessions/features/worklist/accession-worklist.store";
 import { useDebouncedValue } from "@/hooks/use-debounced-value";
+import { Tooltip } from "@nextui-org/react";
 import { CircleIcon, PlusCircle, TimerIcon, XCircle } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { AccessionStatus } from "../../types";
 
 export function AccessionWorklistToolbar() {
@@ -24,19 +27,40 @@ export function AccessionWorklistToolbar() {
   }, [debouncedFilterInput, resetFilters, setFilterInput]);
 
   const createAccession = useNewAccession();
+  useHotkeys("a", () => {
+    createAccession();
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useHotkeys(
+    "f",
+    (event) => {
+      event.preventDefault();
+      inputRef.current?.focus();
+    },
+    { enableOnTags: ["INPUT", "TEXTAREA"] }
+  );
 
   return (
     <div className="flex-col space-y-3 sm:flex-row sm:flex sm:items-center sm:justify-between sm:flex-1 sm:space-y-0">
       <div className="flex items-center justify-between">
         <div className="flex items-center flex-1 space-x-2">
-          <Input
-            placeholder="Filter accessions..."
-            value={(liveValue as string) ?? ""}
-            onChange={(event) => {
-              setLiveValue(event.currentTarget.value);
-            }}
-            className="w-48 lg:w-54"
-          />
+          <div className="relative">
+            <Input
+              ref={inputRef}
+              placeholder="Filter accessions..."
+              value={(liveValue as string) ?? ""}
+              onChange={(event) => {
+                setLiveValue(event.currentTarget.value);
+              }}
+              className="w-48 lg:w-54"
+            />
+            {(liveValue?.length ?? 0) <= 0 && (
+              <span className="absolute text-gray-500 transform -translate-y-1/2 pointer-events-none right-2 top-[45%]">
+                <Kbd command={"F"} />
+              </span>
+            )}
+          </div>
           <FilterControl title="Status" options={statuses} />
           {isFiltered.result() && (
             <Button
@@ -53,13 +77,25 @@ export function AccessionWorklistToolbar() {
           )}
         </div>
       </div>
-      <Button
-        className="w-full space-x-2 sm:w-auto"
-        onClick={() => createAccession()}
+
+      <Tooltip
+        placement="top"
+        closeDelay={0}
+        delay={600}
+        content={
+          <TooltipHotkey>
+            Create New Accession <Kbd command={"A"} />
+          </TooltipHotkey>
+        }
       >
-        <PlusCircle className="w-4 h-4" />
-        <span>Add Accession</span>
-      </Button>
+        <Button
+          className="w-full space-x-2 sm:w-auto"
+          onClick={() => createAccession()}
+        >
+          <PlusCircle className="w-4 h-4" />
+          <span>Add Accession</span>
+        </Button>
+      </Tooltip>
     </div>
   );
 }
