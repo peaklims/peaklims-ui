c1caa83fedd7c74b30802f99bd5ad8285780ab50
Paul DeVito <pdevito3@gmail.com>
2024-06-01 16:05:34 -0400

    feat: combobox sizing

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { Check } from "lucide-react";
 import * as React from "react";
 
 import {
@@ -202,7 +201,7 @@ function Option({ item, state }: OptionProps) {
       {...optionProps}
       ref={ref}
       className={cn(
-        `m-1 flex cursor-default items-center justify-between rounded-md px-2 py-2 text-sm outline-none text-gray-700`,
+        `m-1 flex cursor-default items-center justify-between rounded-md px-2 py-1 text-sm outline-none text-gray-700`,
         isFocused ? "bg-emerald-100 text-emerald-600" : "",
         isSelected ? "font-semibold text-emerald-600" : "",
         isDisabled ? "opacity-40 text-slate-700 cursor-not-allowed" : ""
@@ -210,7 +209,24 @@ function Option({ item, state }: OptionProps) {
     >
       {item.rendered}
       {isSelected && (
-        <Check aria-hidden="true" className="w-5 h-5 text-emerald-600" />
+        // <Check aria-hidden="true" className="w-5 h-5 text-emerald-600" />
+        <svg
+          aria-hidden="true"
+          className="w-4 h-4 text-emerald-600"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M4.5 11.7948L8.72144 16.0163C8.86993 16.1666 9.04677 16.286 9.24172 16.3674C9.43668 16.4488 9.64586 16.4908 9.85715 16.4908C10.0685 16.4908 10.2776 16.4488 10.4726 16.3674C10.6675 16.286 10.8443 16.1666 10.9929 16.0163L19.5 7.50916"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       )}
     </li>
   );
