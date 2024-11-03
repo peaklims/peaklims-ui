f69180b4b05c0f32c7cbf911bb9954c714933438
Paul DeVito <pdevito3@gmail.com>
2024-11-02 21:42:33 -0400

    feat: no org name delay

--- src/domain/peak-organizations/apis/get-peak-organization.tsx
+++ src/domain/peak-organizations/apis/get-peak-organization.tsx
@@ -5,37 +5,18 @@ import { AxiosResponse } from "axios";
 import { PeakOrganizationDto } from "../types";
 import { PeakOrganizationKeys } from "./peakorganization.keys";
 
-interface DelayProps {
-  hasArtificialDelay?: boolean;
-  delayInMs?: number;
-}
-
-export const getPeakOrganization = async (
-  peakOrganizationId: string,
-  { hasArtificialDelay = false, delayInMs = 0 }: DelayProps = {}
-) => {
-  const [data] = await Promise.all([
-    peakLimsApi
-      .get(`/v1/peakorganizations/${peakOrganizationId}`)
-      .then((response: AxiosResponse<PeakOrganizationDto>) => response.data),
-    new Promise((resolve) =>
-      setTimeout(resolve, hasArtificialDelay ? delayInMs : 0)
-    ),
-  ]);
-  return data;
+export const getPeakOrganization = async (peakOrganizationId: string) => {
+  return peakLimsApi
+    .get(`/v1/peakorganizations/${peakOrganizationId}`)
+    .then((response: AxiosResponse<PeakOrganizationDto>) => response.data);
 };
 
 export const useGetPeakOrganization = (
-  peakOrganizationId: string | undefined,
-  { hasArtificialDelay = false, delayInMs = 0 }: DelayProps = {}
+  peakOrganizationId: string | undefined
 ) => {
   return useQuery({
     queryKey: PeakOrganizationKeys.detail(peakOrganizationId!),
-    queryFn: () =>
-      getPeakOrganization(peakOrganizationId!, {
-        hasArtificialDelay,
-        delayInMs,
-      }),
+    queryFn: () => getPeakOrganization(peakOrganizationId!),
     enabled: peakOrganizationId !== null && peakOrganizationId !== undefined,
     gcTime: Infinity,
     staleTime: Infinity,
