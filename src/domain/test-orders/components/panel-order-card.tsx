d5fe8d16e8048ab6409100d728ce1b13ea7b927d
Paul DeVito <pdevito3@gmail.com>
2024-12-01 19:49:41 -0500

    refactor: PO dropdown

--- src/domain/test-orders/components/panel-order-card.tsx
+++ src/domain/test-orders/components/panel-order-card.tsx
@@ -125,8 +125,8 @@ export function PanelOrderCard({
             </h4>
           </button>
 
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
+          <NextDropdown>
+            <NextDropdownTrigger>
               <Button
                 className={cn(
                   "inline-flex items-center px-2 py-2 text-sm font-medium leading-5 transition duration-100 ease-in bg-white rounded-full hover:shadow",
@@ -134,7 +134,6 @@ export function PanelOrderCard({
                   "sm:p-3 dark:hover:shadow dark:shadow-slate-400 dark:hover:shadow-slate-300"
                 )}
               >
-                {/* https://iconbuddy.app/ion/ellipsis-horizontal-sharp */}
                 <svg
                   className="w-4 h-4"
                   width={512}
@@ -147,53 +146,45 @@ export function PanelOrderCard({
                   <circle cx={96} cy={256} r={48} fill="currentColor" />
                 </svg>
               </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" side="right">
-              <DropdownMenuGroup>
-                <DropdownMenuItem
-                  onClick={() => {
-                    removePanelOrderApi
-                      .mutateAsync({
-                        accessionId: accessionId,
-                        panelOrderId: panelOrder.panelOrderId,
-                      })
-                      .catch((e) => {
-                        Notification.error(
-                          "There was an error removing the Panel"
-                        );
-                        console.error(e);
-                      });
-                  }}
-                >
+            </NextDropdownTrigger>
+            <NextDropdownMenu
+              aria-label="Panel Actions"
+              onAction={(key) => {
+                if (key === "remove") {
+                  removePanelOrderApi
+                    .mutateAsync({
+                      accessionId: accessionId,
+                      panelOrderId: panelOrder.panelOrderId,
+                    })
+                    .catch((e) => {
+                      Notification.error(
+                        "There was an error removing the Panel"
+                      );
+                      console.error(e);
+                    });
+                }
+                if (key === "cancel") {
+                  alert(`Cancel Panel Order ${panelOrder.id}`);
+                }
+              }}
+            >
+              <NextDropdownItem key="remove" className="rounded-md">
+                <div className="flex items-center space-x-3">
                   <p>Remove</p>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-              {/* <DropdownMenuSeparator /> */}
-              {/* <DropdownMenuLabel>Manage Sample</DropdownMenuLabel>
-              <DropdownMenuSeparator /> */}
-              <DropdownMenuGroup>
-                <DropdownMenuItem
-                  asChild
-                  onClick={(e) => {
-                    alert(`Cancel Panel Order ${panelOrder.id}`);
-                    e.stopPropagation();
-                  }}
-                >
-                  <p
-                    className={cn(
-                      "hover:bg-rose-200 hover:text-rose-700 hover:outline-none",
-                      "focus:bg-rose-200 focus:text-rose-700 focus:outline-none",
-                      "dark:border-slate-900 dark:bg-slate-800 dark:text-white dark:hover:bg-rose-800 dark:hover:text-rose-300 dark:hover:outline-none",
-                      "dark:hover:shadow dark:shadow-rose-400 dark:hover:shadow-rose-300",
-                      "flex items-center justify-start space-x-2 w-full text-rose-500"
-                    )}
-                  >
-                    Cancel Order
-                  </p>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                </div>
+              </NextDropdownItem>
+
+              <NextDropdownItem key="cancel" className={cn(
+                "rounded-md",
+                "text-rose-500 hover:text-rose-700",
+                "dark:text-rose-400 dark:hover:text-rose-300"
+              )}>
+                <div className="flex items-center space-x-3">
+                  <p>Cancel Order</p>
+                </div>
+              </NextDropdownItem>
+            </NextDropdownMenu>
+          </NextDropdown>
         </div>
         {showPanelTestsId && (
           <TestOrderActions
@@ -242,83 +233,85 @@ function TestOrderActions({
               <div className="flex flex-col w-full pl-2">
                 <div className="flex flex-col pl-2 border-indigo-600 border-l-3">
                   <div className="flex flex-col">
-                    <div className="flex items-center flex-1 space-x-2">
-                      <h5 className="text-sm font-semibold tracking-tight">
-                        {testOrder.testName}
-                      </h5>
-                      <p className="block text-xs font-semibold text-gray-400">
-                        [{testOrder.testCode}]
-                      </p>
-                    </div>
+                    <div className="flex flex-col">
+                      <div className="flex items-center flex-1 space-x-2">
+                        <h5 className="text-sm font-semibold tracking-tight">
+                          {testOrder.testName}
+                        </h5>
+                        <p className="block text-xs font-semibold text-gray-400">
+                          [{testOrder.testCode}]
+                        </p>
+                      </div>
 
-                    <div className="flex flex-row items-center space-x-3">
-                      {testOrder?.dueDate !== null &&
-                        testOrder?.dueDate !== undefined && (
-                          <div className="flex items-center space-x-1">
-                            <Calendar className="w-4 h-4 text-slate-500" />
-                            <p className="text-sm">
-                              {testOrder?.dueDate?.toString()}
-                            </p>
-                          </div>
-                        )}
-                      {testOrder.priority === "STAT" && (
-                        <div className="flex items-center space-x-1">
-                          <p className="text-xs font-semibold text-amber-500">
-                            STAT
-                          </p>
-                          <Stat className="w-4 h-4 text-amber-500" />
-                        </div>
-                      )}
-                    </div>
-
-                    <div className="flex items-center justify-start pt-2 space-x-3">
-                      <TestOrderStatusBadge
-                        status={(testOrder.status || "-") as TestOrderStatus}
-                      />
-                      {testOrder.status === "Cancelled" && (
-                        <CancellationInfoButton
-                          cancellationReason={
-                            testOrder.cancellationReason ?? ""
-                          }
-                          cancellationComments={
-                            testOrder.cancellationComments ?? ""
-                          }
-                        />
-                      )}
-                    </div>
-
-                    <SetSampleModal
-                      testOrderId={testOrder.id}
-                      sampleId={testOrder.sample.id}
-                      testName={testOrder.testName}
-                      patientId={patientId}
-                    >
-                      <SetSampleButton className="inline-flex items-center pt-2 group">
-                        <>
-                          <p className="text-xs font-medium transition-colors group-hover:text-slate-500">
-                            {testOrder.sample.sampleNumber ||
-                              "Sample not assigned"}
-                          </p>
-                          {(testOrder?.sample?.sampleNumber?.length ?? 0) <=
-                            0 && (
-                            <div className="pl-2">
-                              <svg
-                                width="512"
-                                height="512"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="w-4 h-4 text-rose-500"
-                              >
-                                <path
-                                  fill="currentColor"
-                                  d="M10 2c4.42 0 8 3.58 8 8s-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8m1.13 9.38l.35-6.46H8.52l.35 6.46zm-.09 3.36c.24-.23.37-.55.37-.96c0-.42-.12-.74-.36-.97s-.59-.35-1.06-.35s-.82.12-1.07.35s-.37.55-.37.97c0 .41.13.73.38.96c.26.23.61.34 1.06.34s.8-.11 1.05-.34"
-                                />
-                              </svg>
+                      <div className="flex flex-row items-center space-x-3">
+                        {testOrder?.dueDate !== null &&
+                          testOrder?.dueDate !== undefined && (
+                            <div className="flex items-center space-x-1">
+                              <Calendar className="w-4 h-4 text-slate-500" />
+                              <p className="text-sm">
+                                {testOrder?.dueDate?.toString()}
+                              </p>
                             </div>
                           )}
-                        </>
-                      </SetSampleButton>
-                    </SetSampleModal>
+                        {testOrder.priority === "STAT" && (
+                          <div className="flex items-center space-x-1">
+                            <p className="text-xs font-semibold text-amber-500">
+                              STAT
+                            </p>
+                            <Stat className="w-4 h-4 text-amber-500" />
+                          </div>
+                        )}
+                      </div>
+
+                      <div className="flex items-center justify-start pt-2 space-x-3">
+                        <TestOrderStatusBadge
+                          status={(testOrder.status || "-") as TestOrderStatus}
+                        />
+                        {testOrder.status === "Cancelled" && (
+                          <CancellationInfoButton
+                            cancellationReason={
+                              testOrder.cancellationReason ?? ""
+                            }
+                            cancellationComments={
+                              testOrder.cancellationComments ?? ""
+                            }
+                          />
+                        )}
+                      </div>
+
+                      <SetSampleModal
+                        testOrderId={testOrder.id}
+                        sampleId={testOrder.sample.id}
+                        testName={testOrder.testName}
+                        patientId={patientId}
+                      >
+                        <SetSampleButton className="inline-flex items-center pt-2 group">
+                          <>
+                            <p className="text-xs font-medium transition-colors group-hover:text-slate-500">
+                              {testOrder.sample.sampleNumber ||
+                                "Sample not assigned"}
+                            </p>
+                            {(testOrder?.sample?.sampleNumber?.length ?? 0) <=
+                              0 && (
+                              <div className="pl-2">
+                                <svg
+                                  width="512"
+                                  height="512"
+                                  viewBox="0 0 20 20"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="w-4 h-4 text-rose-500"
+                                >
+                                  <path
+                                    fill="currentColor"
+                                    d="M10 2c4.42 0 8 3.58 8 8s-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8m1.13 9.38l.35-6.46H8.52l.35 6.46zm-.09 3.36c.24-.23.37-.55.37-.96c0-.42-.12-.74-.36-.97s-.59-.35-1.06-.35s-.82.12-1.07.35s-.37.55-.37.97c0 .41.13.73.38.96c.26.23.61.34 1.06.34s.8-.11 1.05-.34"
+                                  />
+                                </svg>
+                              </div>
+                            )}
+                          </>
+                        </SetSampleButton>
+                      </SetSampleModal>
+                    </div>
                   </div>
                 </div>
               </div>
