d95a68de86033edf2fd84ca67e939594a04645f9
Paul DeVito <pdevito3@gmail.com>
2025-02-09 22:20:49 -0500

    fix: tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,9 +72,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "npm run start",
-  //   url: "http://127.0.0.1:4378",
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm dev",
+    url: "https:localhost:4378/",
+    reuseExistingServer: !process.env.CI,
+    ignoreHTTPSErrors: true,
+  },
 });
